7c7f0567b6f6fd320a273203b35469a8df15facc
Alessandro Reichert <alessandro.reichert@ciopartner.com.br>
2013-06-25 00:46:44 -0300

    ajuste para o dropbox funcionar legal

---
+++ portal/home/static/home/js/home.js
@@ -0,0 +1,24 @@
+(function() {
+
+    $('.activoverfl').mouseover(function () {
+        $(this).addClass('activoverfl2');
+        $('.foty').fadeIn(0);
+    });
+
+    $('.activoverfl*').mouseleave(function (e) {
+        $('.foty').fadeOut(0);
+        $('.activoverfl').removeClass('activoverfl2');
+    });
+
+
+    $('.terg-hdmgmenu').mouseover(function () {
+        //$(this).addClass('activoverfl2');
+        $('.hdmgmenu').fadeIn(0);
+    });
+
+    $('.terg-hdmgmenu*').mouseleave(function (e) {
+        $('.hdmgmenu').fadeOut(0);
+        //$('.activoverfl').removeClass('activoverfl2');
+    });
+
+}).call(this);
