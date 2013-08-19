e542513a8d0b4bf9086f4711d2fb2c1d3148b26b
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-08-19 11:26:52 -0300

    conversões

--- portal/ferramentas/forms.py
+++ portal/ferramentas/forms.py
@@ -1,10 +1,14 @@
 # coding=utf-8
 from Crypto.Util import asn1
 from datetime import date
+from os import popen2
+import os
 import urllib
 import urlparse
 import OpenSSL
+from OpenSSL.crypto import load_certificate, FILETYPE_PEM, dump_certificate, FILETYPE_ASN1, load_pkcs12, PKCS12, dump_privatekey
 from django.core.exceptions import ValidationError
+from django.core.files.temp import NamedTemporaryFile
 from django.forms import Form, CharField, HiddenInput, Textarea, FileField, ChoiceField, PasswordInput
 import requests
 from hashlib import md5
@@ -144,6 +148,7 @@ class SSLConverterForm(Form):
     )
 
     certificado = FileField()
+    private_key = FileField()
     tipo_atual = ChoiceField(choices=TIPO_CHOICES)
     tipo_para_coverter = ChoiceField(choices=TIPO_CHOICES)
     pfx_password = CharField(widget=PasswordInput)
@@ -159,27 +164,84 @@ class SSLConverterForm(Form):
         return self.cleaned_data
 
     def processa(self):
-        certificado = self.cleaned_data['certificado']
+        certificado = self.cleaned_data['certificado'].read()
+        private_key = self.cleaned_data['private_key'].read()
         tipo_atual = self.cleaned_data['tipo_atual']
         tipo_para_converter = self.cleaned_data['tipo_para_converter']
+        password = self.cleaned_data['pfx_password']
 
+        # PEM -> X
+        if tipo_atual == self.STANDARD_PEM:
+            if tipo_para_converter == self.DER_BINARY:
+                return self.converte_pem_der(certificado)
+            if tipo_para_converter == self.P7B_PKCS_7:
+                return self.converte_pem_p7(certificado)
+            return self.converte_pem_p12(certificado, private_key, password)
 
-        return
+        # DER -> X
+        if tipo_atual == self.DER_BINARY:
+            if tipo_para_converter == self.STANDARD_PEM:
+                return self.converte_der_pem(certificado)
+            if tipo_para_converter == self.P7B_PKCS_7:
+                return self.converte_pem_p7(self.converte_der_pem(certificado))
+            return self.converte_pem_p12(self.converte_der_pem(certificado), private_key, password)
+
+        # P7 -> X
+        if tipo_atual == self.P7B_PKCS_7:
+            if tipo_para_converter == self.STANDARD_PEM:
+                return self.converte_p7_pem(certificado)
+            if tipo_para_converter == self.DER_BINARY:
+                return self.converte_pem_der(self.converte_p7_pem(certificado))
+            return self.converte_pem_p12(self.converte_p7_pem(certificado), private_key, password)
+
+        # P12 -> X
+        if tipo_para_converter == self.STANDARD_PEM:
+            return self.converte_p12_pem(certificado, password)
+        if tipo_para_converter == self.DER_BINARY:
+            return self.converte_pem_der(self.converte_p12_pem(certificado, password))
+        return self.converte_pem_p7(self.converte_p12_pem(certificado, password))
+
+    def _runopenssl(self, comando):
+        write, read = popen2(comando, "b")
+        write.close()
+        return read.read()
 
     def converte_pem_der(self, certificado):
-        pass
+        cert = load_certificate(FILETYPE_PEM, certificado)
+        return dump_certificate(FILETYPE_ASN1, cert)
 
     def converte_pem_p7(self, certificado):
-        pass
+        file_in = NamedTemporaryFile(delete=False)
+        file_in.write(certificado)
+        path_in = file_in.name
+        file_in.close()
+        # Como a biblioteca PyOpenSSL não trata pkcs7, é usado a própria openssl do linux.
+        cert = self._runopenssl('openssl crl2pkcs7 -nocrl -certfile %s' % path_in)
+        os.remove(path_in)
+        return cert
 
-    def converte_pem_p12(self, certificado):
-        pass
+    def converte_pem_p12(self, certificado, private_key, password):
+        p12 = PKCS12()
+        cert = load_certificate(FILETYPE_PEM, certificado)
+        p12.set_certificate(cert)
+        p12.set_privatekey(private_key)
+        return p12.export(password, iter=2, maciter=3)
 
-    def converte_pem_der(self, certificado):
-        pass
+    def converte_der_pem(self, certificado):
+        cert = load_certificate(FILETYPE_ASN1, certificado)
+        return dump_certificate(FILETYPE_PEM, cert)
 
-    def converte_pem_p7(self, certificado):
-        pass
+    def converte_p7_pem(self, certificado):
+        file_in = NamedTemporaryFile(delete=False)
+        file_in.write(certificado)
+        path_in = file_in.name
+        file_in.close()
+        # Como a biblioteca PyOpenSSL não trata pkcs7, é usado a própria openssl do linux.
+        cert = self._runopenssl('openssl pkcs7 -print_certs -in %s' % path_in)
+        os.remove(path_in)
+        return cert
 
-    def converte_pem_p12(self, certificado):
-        pass
+    def converte_p12_pem(self, certificado, password):
+        p12 = load_pkcs12(certificado, password)
+        return dump_certificate(FILETYPE_PEM, p12.get_certificate()) + \
+            dump_privatekey(FILETYPE_PEM, p12.get_privatekey())
