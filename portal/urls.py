e254bf839c956766c2ab37c16ed696e5f4f01b36
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-09-25 17:11:54 -0300

    url admin

--- portal/urls.py
+++ portal/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns(
 
     # Change the admin prefix here to use an alternate URL for the
     # admin interface, which would be marginally more secure.
-    ('^admin-portal/', include(admin.site.urls)),
+    ('^admin/', include(admin.site.urls)),
     (r'^e-commerce/', include(application.urls)),
 
     ('^', include('portal.certificados.urls')),
