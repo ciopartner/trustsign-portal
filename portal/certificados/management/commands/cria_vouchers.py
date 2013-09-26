5fd09fc6238406135e21463eefb63784f16db4cf
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-09-26 12:21:11 -0300

    comando para criar vouchers para testes (cherry picked from commit 101bbcc)

---
+++ portal/certificados/management/commands/cria_vouchers.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+from copy import copy
+from django.core.management import BaseCommand
+from portal.certificados.models import Voucher
+
+
+class Command(BaseCommand):
+    args = '<qtd_vouchers>'
+
+    def handle(self, *args, **options):
+        qtd_vouchers = int(args[0]) if len(args) > 0 else 50
+
+        for crm_hash in ('SSL', 'WILDCARD', 'MDC', 'SAN', 'EV', 'EV-MDC'):
+            voucher_base = Voucher.objects.get(crm_hash=crm_hash)
+
+            for i in range(qtd_vouchers):
+                try:
+                    # remove se já existir
+                    voucher = Voucher.objects.get(crm_hash='%s%s' % (crm_hash, i))
+                    voucher.delete()
+                except Voucher.DoesNotExist:
+                    pass
+
+                voucher = copy(voucher_base)
+                voucher.id = None
+                voucher.crm_hash += str(i)
+                voucher.save()
+
+        self.stdout.write('Criou %d vouchers de cada tipo"' % qtd_vouchers)
