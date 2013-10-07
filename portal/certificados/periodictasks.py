51a041af99cbfc7e498f6df603ebd4bf96052ddb
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-10-07 14:41:22 -0300

    check email

--- portal/certificados/periodictasks.py
+++ portal/certificados/periodictasks.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 from __future__ import unicode_literals
+import email
+import imaplib
+import os
 from django.conf import settings
 from periodically.decorators import every
 from logging import getLogger
@@ -49,3 +52,39 @@ def envia_comodo():
             emissao.emission_error_message = '%s (%s)' % (e.comodo_message, e.code)
 
         emissao.save()
+
+
+def check_email():
+    m = imaplib.IMAP4_SSL("imap.gmail.com")
+    m.login(settings.CERTIFICADOS_EMAIL_USERNAME, settings.CERTIFICADOS_EMAIL_PASSWORD)
+    m.select("[Gmail]/Todos os e-mails")
+    resp, items = m.search(None, "ALL")
+    items = items[0].split()
+    for emailid in items:
+        resp, data = m.fetch(emailid, "(RFC822)")
+        email_body = data[0][1]
+        mail = email.message_from_string(email_body)
+
+        if mail.get_content_maintype() != 'multipart':
+            continue
+
+        counter = 1
+
+        for part in mail.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            if part.get('Content-Disposition') is None:
+                continue
+
+            filename = part.get_filename()
+
+            if not filename:
+                filename = 'part-%03d%s' % (counter, 'bin')
+                counter += 1
+
+            att_path = os.path.join(settings.CERTIFICADOS_EMAIL_PATH_ATTACHMENTS, filename)
+
+            if not os.path.isfile(att_path):
+                fp = open(att_path, 'wb')
+                fp.write(part.get_payload(decode=True))
+                fp.close()
