0b5e5c12bc040c46dcd2e2ed5919a45f753b967d
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-10-16 17:49:44 -0300

    tirando csrf

--- ecommerce/website/urls.py
+++ ecommerce/website/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 from .views import GetCNPJDataView, AdicionarProdutoView
 
 urlpatterns = patterns(
     '',
     url(r'^ajax/get-cnpj-data/$', GetCNPJDataView.as_view()),
-    url(r'^ajax/adicionar-produto/$', AdicionarProdutoView.as_view()),
+    url(r'^ajax/adicionar-produto/$', csrf_exempt(AdicionarProdutoView.as_view())),
 )
