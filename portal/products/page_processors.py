024cd012674595ec09f8172256cc746e6cb2c601
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-10-16 17:13:06 -0300

    preços dos produtos no portal

--- portal/products/page_processors.py
+++ portal/products/page_processors.py
@@ -7,34 +7,49 @@ from django.db import connection
 def ferramentas_processor(request, page):
 
     sql = '''
-    SELECT upc, price_excl_tax FROM
-       catalogue_product
-    inner join partner_stockrecord
-        on partner_stockrecord.product_id = catalogue_product.id where catalogue_product.product_code = %s'''
+    SELECT p.upc, p.product_line, p.product_term, s.price_excl_tax
+    FROM
+       catalogue_product p
+    inner join partner_stockrecord s
+        on s.product_id = p.id
+    where p.product_code = %s'''
 
     cursor = connection.cursor()
     cursor.execute(sql, [page.product.product_code])
 
-    for row in cursor.fetchall():
-        print(row)
-
-    return {
+    data = {
         'product_code': 'ssl',
         'precos': {
             'basic': {
-                'term1year': [['1000','20'],['2000','40'],['3000','60']],
-                'term2years': ['1500','20'],
-                'term3years': ['2250','20']
+                'term1year': {},
+                'term2years': {},
+                'term3years': {},
             },
             'pro': {
-                'term1year': [['1340','20'],['2680','40'],['4020','60']],
-                'term2years': ['2310','20'],
-                'term3years': ['2510','20']
+                'term1year': {},
+                'term2years': {},
+                'term3years': {},
             },
             'prime': {
-                'term1year': [['6010','20'],['12020','40'],['36030','60']],
-                'term2years':['11010','20'],
-                'term3years': ['21040','20']
+                'term1year': {},
+                'term2years': {},
+                'term3years': {},
             }
         }
     }
+
+    for upc, product_line, product_term, price in cursor.fetchall():
+        price = price.quantize(Decimal('0.01'))
+        x = data['precos'][product_line]['term%s' % product_term]
+        x['price_tpl'] = str(price).split('.')
+        x['price'] = price
+
+    for line in ('basic', 'pro', 'prime'):
+        data_line = data.setdefault('precos', {})[line]
+        data_line.setdefault('term1year', {})['discount'] = 0
+        preco_1ano = data_line.get('term1year', {}).get('price', 0)
+        if preco_1ano > 0:
+            data_line['term2years']['discount'] = int((1 - data_line.get('term2years', {}).get('price', 0) / 2 * preco_1ano) * 100)
+            data_line['term3years']['discount'] = int((1 - data_line.get('term3years', {}).get('price', 0) / 3 * preco_1ano) * 100)
+
+    return data
