9f97f01baed472a0e935dfbd3e94e7e8f803f809
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-10-18 12:49:25 -0300

    static_urls no settings

--- ecommerce/website/middlewares.py
+++ ecommerce/website/middlewares.py
@@ -1,17 +1,10 @@
-import re
- 
-from django.utils.text import compress_string
-from django.utils.cache import patch_vary_headers
- 
 from django import http
- 
-try:
-    import settings 
-    XS_SHARING_ALLOWED_ORIGINS = settings.XS_SHARING_ALLOWED_ORIGINS
-    XS_SHARING_ALLOWED_METHODS = settings.XS_SHARING_ALLOWED_METHODS
-except:
-    XS_SHARING_ALLOWED_ORIGINS = '*'
-    XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
+from django.conf import settings
+
+XS_SHARING_ALLOWED_ORIGINS = getattr(settings, 'XS_SHARING_ALLOWED_ORIGINS', '*')
+XS_SHARING_ALLOWED_METHODS = getattr(settings,
+                                     'XS_SHARING_ALLOWED_METHODS',
+                                     ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'])
  
  
 class XsSharing(object):
@@ -26,8 +19,8 @@ class XsSharing(object):
  
         if 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
             response = http.HttpResponse()
-            response['Access-Control-Allow-Origin']  = XS_SHARING_ALLOWED_ORIGINS 
-            response['Access-Control-Allow-Methods'] = ",".join( XS_SHARING_ALLOWED_METHODS ) 
+            response['Access-Control-Allow-Origin'] = XS_SHARING_ALLOWED_ORIGINS
+            response['Access-Control-Allow-Methods'] = ','.join(XS_SHARING_ALLOWED_METHODS)
             
             return response
  
@@ -38,7 +31,7 @@ class XsSharing(object):
         if response.has_header('Access-Control-Allow-Origin'):
             return response
  
-        response['Access-Control-Allow-Origin']  = XS_SHARING_ALLOWED_ORIGINS 
-        response['Access-Control-Allow-Methods'] = ",".join( XS_SHARING_ALLOWED_METHODS )
+        response['Access-Control-Allow-Origin'] = XS_SHARING_ALLOWED_ORIGINS
+        response['Access-Control-Allow-Methods'] = ','.join(XS_SHARING_ALLOWED_METHODS)
  
         return response
--- portal/settings.py
+++ portal/settings.py
@@ -136,7 +136,7 @@ CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_DIRNAME
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/static/"
+STATIC_URL = "/portal/static/"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
