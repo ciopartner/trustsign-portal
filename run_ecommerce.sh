6c3e4536499499dd0ba22249f6a64394d9403e35
Alessandro Reichert <alessandro.reichert@ciopartner.com.br>
2013-10-18 17:51:48 -0300

    Atualizações para homologação

--- run_ecommerce.sh
+++ run_ecommerce.sh
@@ -2,18 +2,18 @@
 source /usr/local/bin/virtualenvwrapper.sh
 source ~/.profile
 source ~/.bashrc
-export DJANGO_SETTINGS_MODULE='ecommerce.settings'
+export DJANGO_SETTINGS_MODULE='settings'
 export LANG=pt_BR.UTF-8
-workon trustsign-portal 
+export TRUSTSIGN_ENVIRONMENT='QAS'
+workon trustsign-portal
 
 git pull
-ecommerce/manage.py collectstatic --noinput
+ecommerce/manage.py collectstatic --noinput --settings=settings
 
 # Try to kill me!
 kill $(cat /tmp/trustsign-ecommerce.pid)
 
-./ecommerce/manage.py runfcgi socket=/tmp/trustsign-ecommerce.sock pidfile=/tmp/trustsign-ecommerce.pid maxrequests=2
+./ecommerce/manage.py runfcgi socket=/tmp/trustsign-ecommerce.sock pidfile=/tmp/trustsign-ecommerce.pid maxrequests=5 minspare=3 maxspare=3 maxchildren=8 --settings=settings
 
-chmod g+w /tmp/trustsign-ecommerce.sock
-chgrp www-data /tmp/trustsign-ecommerce.sock
-
+sudo chmod g+w /tmp/trustsign-ecommerce.sock
+sudo chgrp www-data /tmp/trustsign-ecommerce.sock
--- run_portal.sh
+++ run_portal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source /usr/local/bin/virtualenvwrapper.sh
-source ~/.profile
 source ~/.bashrc
 export DJANGO_SETTINGS_MODULE='settings'
+export TRUSTSIGN_ENVIRONMENT='QAS'
 export LANG=pt_BR.UTF-8
-workon trustsign-portal 
+workon trustsign-portal
 
 git pull
 ./portal/manage.py collectstatic --noinput
@@ -12,8 +12,7 @@ git pull
 # Try to kill me!
 kill $(cat /tmp/trustsign-portal.pid)
 
-./portal/manage.py runfcgi socket=/tmp/trustsign-portal.sock pidfile=/tmp/trustsign-portal.pid maxrequests=2 --settings=settings
+./portal/manage.py runfcgi socket=/tmp/trustsign-portal.sock pidfile=/tmp/trustsign-portal.pid maxrequests=5 minspare=3 maxspare=3 maxchildren=8 --settings=settings
 
-chmod g+w /tmp/trustsign-portal.sock
-chgrp www-data /tmp/trustsign-portal.sock
-
+sudo chmod g+w /tmp/trustsign-portal.sock
+sudo chgrp www-data /tmp/trustsign-portal.sock
