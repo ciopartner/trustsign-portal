eb82b0ecc134d5cb226be33b501ffb6696efbbc2
Carlos Maniero <carlos.maniero@ciopartner.com.br>
2013-10-21 16:03:35 -0200

    Telas de pagamento

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -200,7 +200,7 @@ OSCAR_ORDER_STATUS_PIPELINE = {
 }
 
 AUTH_PROFILE_MODULE = "home.TrustSignProfile"
-LOGIN_URL = "/ecommerce/customer/login"
+LOGIN_URL = "/ecommerce/accounts/login"
 
 # Sobrescreva com os settings globais
 from settings_global import *
