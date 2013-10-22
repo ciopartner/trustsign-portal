23d7cf4cf03172acc9ad9caa8888d5238eeb4ce8
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-10-22 20:03:18 -0200

    admin ecommerce order

--- ecommerce/apps/order/processing.py
+++ ecommerce/apps/order/processing.py
@@ -5,7 +5,6 @@ from libs.crm.mixins import OscarToCRMMixin
 class EventHandler(CoreEventHandler, OscarToCRMMixin):
     def handle_shipping_event(self, order, event_type, lines, line_quantities, **kwargs):
         self.validate_shipping_event(order, event_type, lines, line_quantities, **kwargs)
-        print 'entroooooooooooooooooooou'
         #self.send_order_to_crm(order)
         self.create_shipping_event(order, event_type, lines, line_quantities, **kwargs)
 
