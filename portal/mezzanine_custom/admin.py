92a3485c70714c7bb6b592ccb30aba80fa866ab1
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-10-22 17:44:25 -0200

    customizando admin mezzanine

---
+++ portal/mezzanine_custom/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from mezzanine.blog.models import BlogPost
+from mezzanine.blog.admin import BlogPostAdmin as CoreBlogPostAdmin
+
+class BlogPostAdmin(CoreBlogPostAdmin):
+    def __init__(self, *args, **kwargs):
+        super(BlogPostAdmin, self).__init__(*args, **kwargs)
+        self.fieldsets[0][1]["fields"].insert(-2, "user")
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        formfield = super(BlogPostAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        if db_field.name == "user":
+            formfield.initial = request.user.id
+        return formfield
+
+admin.site.unregister(BlogPost)
+
+admin.site.register(BlogPost, BlogPostAdmin)
