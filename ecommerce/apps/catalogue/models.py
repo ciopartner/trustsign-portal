9d51bd58ed8b820ceb91a1ba19fa60e5ab94ae22
Alessandro <alessandro.reichert@ciopartner.com.br>
2013-10-23 19:46:55 -0200

    Removendo atributos do produto e colocando na classe de produtos - readicionando os cmapos da classe produto

--- ecommerce/apps/catalogue/models.py
+++ ecommerce/apps/catalogue/models.py
@@ -5,10 +5,9 @@ from oscar.apps.catalogue.abstract_models import AbstractProduct
 
 
 class Product(AbstractProduct):
-    #product_code = CharField('Código do Produto', max_length=16, blank=True, null=True)
-    #product_line = CharField('Linha do Produto', max_length=16, blank=True, null=True)
-    #product_term = CharField('Vigência do Produto', max_length=16, blank=True, null=True)
-    #product_crm_code = CharField('Código no CRM', max_length=32, blank=True, null=True)
+    product_code = CharField('Código do Produto', max_length=16, blank=True, null=True)
+    product_line = CharField('Linha do Produto', max_length=16, blank=True, null=True)
+    product_term = CharField('Vigência do Produto', max_length=16, blank=True, null=True)
 
     def get_absolute_url(self):
         return '/portal/certificado-digital/%s/' % self.product_code
