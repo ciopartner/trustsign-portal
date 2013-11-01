b0234b390017fbbd79e845954f94336f08a3d790
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-11-01 09:53:52 -0200

    codigo sem linha form de adicionar no carrinho

--- ecommerce/website/forms.py
+++ ecommerce/website/forms.py
@@ -4,5 +4,5 @@ from ecommerce.certificados.models import Voucher
 
 
 class AdicionarProdutoForm(AddToBasketForm):
-    line = ChoiceField(choices=Voucher.LINHA_CHOICES)
+    line = ChoiceField(choices=Voucher.LINHA_CHOICES + (('na', 'Sem linha'),))
     term = ChoiceField(choices=Voucher.VALIDADE_CHOICES)
