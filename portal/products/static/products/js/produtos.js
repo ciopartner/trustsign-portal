0454d1010f1e3f749a1058b9d598884b3d2f37e7
Carlos Maniero <carlosmaniero@gmail.com>
2013-11-07 15:21:58 -0200

    Dominios adicionais

--- portal/products/static/products/js/produtos.js
+++ portal/products/static/products/js/produtos.js
@@ -2,11 +2,14 @@ $(document).ready(function () {
     var $qtd_carrinho = $('#qtd_carrinho');
     var $message = $("#comprar-message");
 
-    function add_product(product_code, line, term, quantity){
+    function add_product(product_code, line, term, quantity, num_domains){
+
+        num_domains = num_domains ? num_domains : 0;
 
         function add_success(data){
             $qtd_carrinho.text(parseInt($qtd_carrinho.text()) + 1);
-            location.href = url_ecommerce + 'basket/';
+            //location.href = url_ecommerce + 'basket/';
+            $message.html("<div class=\"alert alert-success\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button>Foi adicionado ao seu carrinho " + quantity + ". <a href='" + url_ecommerce + "/basket'>Clique aqui</a> para visualizá-lo.</div>");
         }
 
         function add_fail(data){
@@ -14,16 +17,45 @@ $(document).ready(function () {
             console.log(data);
         }
 
+        function show_buy_info(){
+            var form = $("#form-confirm-buy");
+
+            form.hide();
+            form.removeClass('hidden');
+            form.slideDown(200);
+
+            form.find('.line').text(line);
+            form.find('.term').text(term.replace('year', ' ano'));
+
+            $("#btn-confirm-buy").bind('click')
+            $("#btn-confirm-buy").on('click', function(e){
+                e.preventDefault();
+
+                qtd = form.find('[name=qtd]').val();
+                num_domains = form.find('[name=num_domains]').val();
+
+                do_post();
+            });
+        }
+
+        function do_post(){
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: url_ecommerce + 'ajax/adicionar-produto/',
+                data: {product_code: product_code, quantity:quantity, line: line, term: term, num_domains : num_domains},
+                success: add_success,
+                error: add_fail
+            });
+        }
+
         quantity = typeof quantity !== 'undefined' ? quantity : 1;
 
-        $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            url: url_ecommerce + 'ajax/adicionar-produto/',
-            data: {product_code: product_code, quantity:quantity, line: line, term: term},
-            success: add_success,
-            error: add_fail
-        });
+        if($("#form-confirm-buy").length != 1){
+            do_post();
+        }else{
+            show_buy_info();
+        }
     }
 
     $('#add-ssl-basic-1year').click(function(){
@@ -35,4 +67,5 @@ $(document).ready(function () {
         e.preventDefault();
         add_product($(this).data('comprar-produto'),$(this).data('line'),$(this).data('term'));
     });
+
 });
