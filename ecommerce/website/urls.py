724165b41033767ff4c870163c08a0c7baf36be6
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-11-12 14:30:38 -0200

    akatus status changed

--- ecommerce/website/urls.py
+++ ecommerce/website/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 from django.views.decorators.csrf import csrf_exempt
+from ecommerce.apps.checkout.akatus.views import StatusChangedView
 from .views import GetCNPJDataView, AdicionarProdutoView
 
 urlpatterns = patterns(
     '',
     url(r'^ajax/get-cnpj-data/$', GetCNPJDataView.as_view()),
     url(r'^ajax/adicionar-produto/$', csrf_exempt(AdicionarProdutoView.as_view())),
-    url(r'^api/v1/atualiza-status-pagto/$', csrf_exempt()),
+    url(r'^api/v1/atualiza-status-pagto/$', csrf_exempt(StatusChangedView.as_view())),
 )
