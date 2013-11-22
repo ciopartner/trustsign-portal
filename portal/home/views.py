6e6b36182bf534cb1f034777ffc08473e2bdaac3
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2013-11-22 11:26:19 -0200

    login portal

--- portal/home/views.py
+++ portal/home/views.py
@@ -1,6 +1,8 @@
-from django.contrib.messages import info
+from django.conf import settings
 from django.contrib.auth import login as auth_login
-from mezzanine.utils.urls import login_redirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import resolve_url
+from django.utils.http import is_safe_url
 from mezzanine.utils.views import render
 from django.utils.translation import ugettext as _
 from .forms import LoginForm
@@ -12,9 +14,15 @@ def login(request, template="accounts/login.html"):
     """
     form = LoginForm(request.POST or None)
     if request.method == "POST" and form.is_valid():
+
+        redirect_to = request.REQUEST.get('next', '')
+        # Ensure the user-originating redirection url is safe.
+        if not is_safe_url(url=redirect_to, host=request.get_host()):
+            redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+
         authenticated_user = form.save()
-        info(request, _("Successfully logged in"))
         auth_login(request, authenticated_user)
-        return login_redirect(request)
+
+        return HttpResponseRedirect(redirect_to)
     context = {"form": form, "title": _("Log in")}
     return render(request, template, context)
