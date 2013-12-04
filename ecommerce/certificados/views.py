1410bee7928b3212cc0fa6778123db9aee79864c
Alessandro <alessandro.reichert@ciopartner.com.br>
2013-12-04 21:05:22 -0200

    Deixando o CRM gravar qualquer voucher

--- ecommerce/certificados/views.py
+++ ecommerce/certificados/views.py
@@ -355,7 +355,8 @@ class VoucherCreateAPIView(CreateModelMixin, AddErrorResponseMixin, GenericAPIVi
             if serializer.is_valid():
                 self.pre_save(serializer.object)
                 # TODO: retirar if abaixo depois que o CRM acertar a API
-                if serializer.object.order_number and serializer.object.order_number.isdigit():
+                # if serializer.object.order_number and serializer.object.order_number.isdigit():
+                if True:
                     self.object = serializer.save(force_insert=True)
                     self.post_save(self.object, created=True)
                     headers = self.get_success_headers(serializer.data)
