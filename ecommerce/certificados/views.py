f51ac5b41e5829cebe71229536da70867b15224d
Gleber Diniz <gleber.cadastros@gmail.com>
2013-12-05 17:44:29 -0200

    Corrigindo bug seleção de voucher

--- ecommerce/certificados/views.py
+++ ecommerce/certificados/views.py
@@ -547,10 +547,7 @@ class EscolhaVoucherView(ListView):
     context_object_name = 'vouchers'
 
     def get_queryset(self):
-        qs = Voucher.objects.select_related('emissao').exclude(
-            emissao__emission_status__in=(
-                Emissao.STATUS_REVOGADO, Emissao.STATUS_EXPIRADO, Emissao.STATUS_OCORREU_ERRO_COMODO
-            ))
+        qs = Voucher.objects.select_related('emissao')
 
         user = self.request.user
         if not user.is_superuser and user.get_profile().is_cliente:
