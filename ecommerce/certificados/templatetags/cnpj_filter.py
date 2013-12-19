7cf3f1cd2899ccb244015a93e178ecb2d8716394
Carlos Maniero <carlosmaniero@gmail.com>
2013-12-19 17:09:57 -0200

    Template de escolha voucher

---
+++ ecommerce/certificados/templatetags/cnpj_filter.py
@@ -0,0 +1,7 @@
+from django import template
+register = template.Library()
+
+
+@register.filter
+def format_cnpj(cnpj):
+    return "%s.%s.%s/%s-%s" % ( cnpj[0:2], cnpj[2:5], cnpj[5:8], cnpj[8:12], cnpj[12:14] )
