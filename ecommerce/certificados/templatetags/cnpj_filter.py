3136774c111eae9799c82301823486048ffcc6a3
Gleber Diniz <gleber.diniz@ciopartner.com.br>
2014-01-14 13:38:53 -0200

    load no template

--- ecommerce/certificados/templatetags/cnpj_filter.py
+++ ecommerce/certificados/templatetags/cnpj_filter.py
@@ -4,4 +4,7 @@ register = template.Library()
 
 @register.filter
 def format_cnpj(cnpj):
-    return "%s.%s.%s/%s-%s" % ( cnpj[0:2], cnpj[2:5], cnpj[5:8], cnpj[8:12], cnpj[12:14] )
+    if len(cnpj) == 14:
+        return "%s.%s.%s/%s-%s" % (cnpj[0:2], cnpj[2:5], cnpj[5:8], cnpj[8:12], cnpj[12:14])
+
+    return cnpj
